f1e3059b66090c73cc8c53216152617736c851a2
fakhrul <fakhrullah@fajarhac.com>
2016-08-12 16:04:00 +0800

    Setup main route for Nasihat API
    Write usable CRUD routes that return json for nasihat.

--- app.js
+++ app.js
@@ -1,8 +1,63 @@
 var express = require('express')
+var bodyParser = require('body-parser')
 var app = express()
+var ApiV1 = '/api/v1'
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
+/*
+ * use body parser to get data from request
+ */
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+/*
+ * API routes
+ */
+app.get(ApiV1 + '/nasihat/:id', function (req, res) {
+
+  var id = req.params['id']
+
+  console.log('show nasihat at id ' + id)
+
+  res.json({
+    id: id,
+    name: 'miaw'
+  })
+
+})
+
+app.put(ApiV1 + '/nasihat/:id', function (req, res) {
+
+  var id = req.params['id']
+  var name = req.body['name']
+
+  console.log('update nasihat at id ' + id)
+
+  res.json({
+    id: id,
+    name: name
+  })
+
+})
+
+app.post(ApiV1 + '/nasihat/', function (req, res){
+
+  console.log('create new nasihat')
+
+  res.json({
+    id: 1
+  })
+
+})
+
+app.delete(ApiV1 + '/nasihat/:id', function (req, res) {
+
+  var id = req.params['id']
+
+  console.log('delete nasihat at id ' + id)
+
+  res.json({
+    id: id
+  })
 })
 
 app.listen(3000, function () {
