831344a63e5e51c0a7283588d2667b85cd8ad427
fakhrul <fakhrullah@fajarhac.com>
2016-08-29 21:59:46 +0800

    Add empty modal for menu
    Add about and nasihatbot modal. Add button to close each modal.

--- public/js/main.js
+++ public/js/main.js
@@ -2,13 +2,44 @@
 
   var menuToggle = document.getElementById('menu-toggle');
   var menu = document.getElementById('menu');
+
+  // TODO use only ONE modal for all modal-type-page
+  
+  var aboutToggle = document.getElementById('about-toggle');
+  var aboutModal = document.getElementById('about-modal');
+
+  var nasihatBotToggle = document.getElementById('nasihat-bot-toggle');
+  var nasihatBotModal = document.getElementById('nasihat-bot-modal');
+
+  var modalNavClose = document.getElementsByClassName('modal-nav-close');
+
   menuToggle.addEventListener('click', function(){
     // console.log('#menu-toggle clicked');
     
     toggleClass(menu, 'hidden');
   })
 
+  aboutToggle.addEventListener('click', function () {
+    console.log('#about-toggle clicked');
+    toggleClass(aboutModal, 'hidden');
+  })
 
+  nasihatBotToggle.addEventListener('click', function () {
+    console.log('#nasihat-bot-toggle clicked');
+    toggleClass(nasihatBotModal, 'hidden');
+  })
+
+  Array.prototype.forEach.call(modalNavClose, function(el){
+    el.addEventListener('click', function(){
+      el.parentNode.className += ' hidden';
+    })
+  })
+
+  /**
+   * Toggle class for element
+   * @param  {DOM} el        DOM element
+   * @param  {string} className name for toggle class         
+   */
   function toggleClass(el, className){
     if (el.classList) {
       el.classList.toggle(className);
