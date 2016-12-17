1d673277d06620fd1a5090a0bdb1b6ec958f2e12
fakhrul <fakhrullah@fajarhac.com>
2016-12-17 11:48:41 +0800

    Refactor homepage routes to it own route file
    Refactoring by removing homepage route from app.js to it own route file in
    routes/ directory.
    
    Why?
    Before I only think of a main route, so I think using routes folder only
    complicated things. But now, I am going to add API routes to use by
    website for background loading and I am thinking of mobile and desktop app
    too.

--- app.js
+++ app.js
@@ -27,64 +27,8 @@ app.use(express.static(path.join(__dirname, '/public')))
 app.set('views', path.join(__dirname, '/views'))
 app.set('view engine', 'pug')
 
-/**
- * Homepage route
- * Show a quote for today with next and previous link
- * Today quote is determined by data in cookie/localstorage
- */
-app.get('/', function (req, res) {
-  console.log('show a quote for today')
-
-  var todayId
-  var todayDate = moment().format('YYYY-MM-DD')
-  console.log(todayDate)
-
-  // Show next quote compare to yesterday
-  var lastQuoteId = req.cookies.lastQuoteId
-  var lastDayQuote = req.cookies.lastDayQuote // date when last quote shown
-
-  if (!lastQuoteId) {
-    // For first time user or first time device
-    console.log('1st time user')
-
-    todayId = 1
-    res.cookie('lastQuoteId', 1)
-    res.cookie('lastDayQuote', todayDate)
-  } else {
-    // Repeated user
-    console.log('Repeated user')
-
-    if (moment(lastDayQuote).isSame(todayDate)) {
-      // Repeated user on same day get same quote as last shown
-      todayId = parseInt(lastQuoteId)
-      console.log('Repeated user same day')
-    } else {
-      // Repeated user different day get to see next quote compared to last shown
-      todayId = parseInt(lastQuoteId) + 1
-
-      res.cookie('lastQuoteId', todayId)
-      res.cookie('lastDayQuote', todayDate)
-
-      console.log('Repeated user different day')
-    }
-  }
-
-  console.log('today quote id = ' + todayId)
-
-  // get quote from database
-  nasihatCol.getNasihatById(todayId, function (err, nasihat) {
-    // TODO if err to next error page
-    if (err) console.log('ERROR: ' + err.message)
-
-    // render page
-    res.render('homepage', {
-      title: 'Nasihat',
-      quoteId: todayId,
-      quote: nasihat.text,
-      source: nasihat.source
-    })
-  })
-})
+// Routes
+app.use('/', require('./routes/index'))
 
 /**
  * Block all api until authentication/authorization added only on production
---
+++ routes/index.js
@@ -0,0 +1,69 @@
+/**
+ * Route for root / homepage
+ */
+
+var express = require('express')
+var router = express.Router()
+var nasihatCol = require('../nasihat_col.js')
+var moment = require('moment')
+
+/**
+ * Homepage route
+ * Show a quote for today with next and previous link
+ * Today quote is determined by data in cookie/localstorage
+ */
+router.get('/', function (req, res) {
+  console.log('show a quote for today')
+
+  var todayId
+  var todayDate = moment().format('YYYY-MM-DD')
+  console.log(todayDate)
+
+  // Show next quote compare to yesterday
+  var lastQuoteId = req.cookies.lastQuoteId
+  var lastDayQuote = req.cookies.lastDayQuote // date when last quote shown
+
+  if (!lastQuoteId) {
+    // For first time user or first time device
+    console.log('1st time user')
+
+    todayId = 1
+    res.cookie('lastQuoteId', 1)
+    res.cookie('lastDayQuote', todayDate)
+  } else {
+    // Repeated user
+    console.log('Repeated user')
+
+    if (moment(lastDayQuote).isSame(todayDate)) {
+      // Repeated user on same day get same quote as last shown
+      todayId = parseInt(lastQuoteId)
+      console.log('Repeated user same day')
+    } else {
+      // Repeated user different day get to see next quote compared to last shown
+      todayId = parseInt(lastQuoteId) + 1
+
+      res.cookie('lastQuoteId', todayId)
+      res.cookie('lastDayQuote', todayDate)
+
+      console.log('Repeated user different day')
+    }
+  }
+
+  console.log('today quote id = ' + todayId)
+
+  // get quote from database
+  nasihatCol.getNasihatById(todayId, function (err, nasihat) {
+    // TODO if err to next error page
+    if (err) console.log('ERROR: ' + err.message)
+
+    // render page
+    res.render('homepage', {
+      title: 'Nasihat',
+      quoteId: todayId,
+      quote: nasihat.text,
+      source: nasihat.source
+    })
+  })
+})
+
+module.exports = router;
