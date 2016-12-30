60780f1db1b795d2338658e1457af24a67b4bf22
fakhrul <fakhrullah@fajarhac.com>
2016-12-30 18:03:18 +0800

    Add url to show nasihat using API url
    GET /nasihat/:id
    Above url now show nasihat by it id. Better it use API url.
    
    Why? Because this is good URL for SEO and so that user can share url for
    certain quote (nasihat).

--- routes/nasihat.js
+++ routes/nasihat.js
@@ -1,3 +1,4 @@
+'use strict'
 /**
  * Routes for nasihat (quotes) resources
  */
@@ -5,6 +6,30 @@
 var express = require('express')
 var router = express.Router()
 var nasihatCol = require('../nasihat_col.js')
+// var nasihatCollection = require('../db/nasihat_collection.js')
+var request = require('request')
+
+/**
+ * GET /nasihat/:id
+ */
+router.get('/:id', (req, res, next) => {
+  var apiUrl = 'http://localhost:3000' + '/api/v1/nasihat/' + req.params.id
+
+  request.get(apiUrl, (err, response, body) => {
+    if (err) {
+      console.log(err)
+      next(err)
+    }
+    res.json(JSON.parse(body))
+  })
+  // nasihatCollection.getNasihatById(id)
+  //   .then(result => {
+  //     // res.render('nasihat/index')
+  //     res.json(result)
+  //   })
+  //   .catch(err => console.log(err))
+})
+router.get('/:id/:slug', (req, res) => res.redirect('/nasihat/' + parseInt(req.params.id)))
 
 /**
  * Get next nasihat
